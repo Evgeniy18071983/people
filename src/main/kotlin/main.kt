27c1c722b8e2533b7b5f2f4871769db688637115
Evgeniy Nechaev <kit8@bk.ru>
2021-10-22 00:26:13 +0300

    git init

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var likes =61
+    var lastNumber = likes % 10
+    if (lastNumber == 1 && likes!=11) {
+            println("Понравилось $likes человеку")
+        }
+        else {
+            println("Понравилось $likes людям")
+
+        }
+    }
